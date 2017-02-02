6a3175bc87fe178746a4e839a610f51f9e991ec7
Ian Sinnott <ian@iansinnott.com>
2017-02-01 22:58:57 -0800

    Add reference link to ramda implementation of curryN

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,10 @@
 /**
  * NOTE: recursiveArgs is used in the recursive case, but will generally not be
  * used in the initial invocation by the user
+ *
+ * Although this is not taken from Ramda their original approach might be worth
+ * a look if refactoring:
+ * https://github.com/ramda/ramda/blob/v0.5.0/ramda.js#L297-L309
  */
 export const curryN = (n, f, recursiveArgs = []) => (...innerArgs) => {
   const curryArgs = recursiveArgs.concat(innerArgs);
